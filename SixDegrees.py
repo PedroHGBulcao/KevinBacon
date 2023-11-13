3c6599c9490a984d9c9cf178a20d19ad1a8a0e94
Pedro <pedrohgbulcao@gmail.com>
2023-11-13 12:23:49 -0300

    agr vai

--- SixDegrees.py
+++ SixDegrees.py
@@ -1,33 +1,39 @@
 import pandas as pd
-import networkx as nx
 
 movies = pd.read_csv("netflix_titles.csv")
 movies = movies.dropna(subset='cast')
-costars = []
+costars = {}
 titles = {}
-actor_names = set()
-G = nx.Graph()
+vis = {}
 for cast, title in movies[['cast', 'title']].itertuples(index=False):
     actors = cast.split(", ")
     for actor in actors:
-        actor_names.add(actor)
+        costars.setdefault(actor, set())
         for actor1 in actors:
             if not actor == actor1:
-                costars.append([actor, actor1])
+                costars[actor].add(actor1)
                 titles[(actor, actor1)] = title
-
-G.add_edges_from(costars)
 
 while True:
     actor = input("Choose an actor/actress: ")
-    if actor not in actor_names:
+    if actor not in costars.keys():
         print("Invalid name!")
     else:
         break
 
-ans = nx.shortest_path(G, source="Kevin Bacon", target=actor)
-print(ans)
-queue = [[("Kevin Bacon", "")]]
+ans = []
+queue = [["Kevin Bacon"]]
+while queue:
+    path = queue.pop(0)
+    actor1 = path[-1]
+    vis[actor1] = True
+    for adj in costars.get(actor1, []):
+        if adj == actor:
+            ans = path + [adj]
+            break
+        if not vis.get(adj, False): queue.append(path + [adj])
+    if ans:
+        break
 
 if not ans:
     print("Actor/Actress not connected to Kevin Bacon\n\n")
@@ -40,5 +46,5 @@ else:
             print(actor1.center(40))
         else:
             actor2 = ans[i+1]
-            title = titles[(actor1, actor2)]
+            title = titles.get((actor1, actor2), "")
             print(actor1.center(40) + "\n" + "was in".center(40) + "\n" + title.center(40) + "\n" + "with".center(40))
